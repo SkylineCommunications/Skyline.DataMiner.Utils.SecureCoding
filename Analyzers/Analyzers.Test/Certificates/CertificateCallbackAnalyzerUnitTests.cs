e6ab8cb778d3764b260e53075d482b4f1b8831b0
Flávio Jacinto <flavio.jacinto@skyline.be>
2024-06-20 17:08:02 +0100

    Added CertificateCallbackAnalyzer + Unit Tests.

---
+++ Analyzers/Analyzers.Test/Certificates/CertificateCallbackAnalyzerUnitTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.CodeAnalysis.Testing;
+using Microsoft.CodeAnalysis;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Skyline.DataMiner.Utils.SecureCoding.Analyzers.Certificates;
+
+#pragma warning disable S2699 // Tests should include assertions is not valid for Roslyn Analyzers Unit Tests.  
+namespace Skyline.DataMiner.Utils.SecureCoding.Analyzers.Tests.SecureIO
+{
+    [TestClass]
+    public class CertificateCallbackAnalyzerUnitTests
+    {
+        private readonly string testCase = File.ReadAllText(@"..\..\..\Certificates\TestScenarios\CertificateCallbackAnalyzer.test");
+        private readonly string testCaseValid = File.ReadAllText(@"..\..\..\Certificates\TestScenarios\CertificateCallbackAnalyzerNoDiagnostic.test");
+
+        [TestMethod]
+        public async Task VerifyUsageDiagnostic()
+        {
+            var expectedDiagnostics = new List<DiagnosticResult>()
+            {
+                AnalyzerVerifierHelper.BuildDiagnosticResult(CertificateCallbackAnalyzer.DiagnosticId, DiagnosticSeverity.Warning, 14, 22),
+                AnalyzerVerifierHelper.BuildDiagnosticResult(CertificateCallbackAnalyzer.DiagnosticId, DiagnosticSeverity.Warning, 22, 66),
+            };
+
+            var analyzerVerifier = AnalyzerVerifierHelper.BuildAnalyzerVerifier<CertificateCallbackAnalyzer>(testCase);
+
+            analyzerVerifier.TestState.ExpectedDiagnostics.AddRange(expectedDiagnostics);
+
+            await analyzerVerifier.RunAsync();
+        }
+
+        [TestMethod]
+        public async Task VerifyUsageDiagnostic_NoDiagnosticsThrown()
+        {
+            var analyzerVerifier = AnalyzerVerifierHelper.BuildAnalyzerVerifier<CertificateCallbackAnalyzer>(testCaseValid);
+
+            await analyzerVerifier.RunAsync();
+        }
+    }
+}
+#pragma warning restore S2699 // Tests should include assertions is not valid for Roslyn Analyzers Unit Tests.
---
+++ Analyzers/Analyzers/Certificates/CertificateCallbackAnalyzer.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace Skyline.DataMiner.Utils.SecureCoding.Analyzers.Certificates
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class CertificateCallbackAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "SLC_SC0005";
+
+        public static DiagnosticDescriptor Rule => new DiagnosticDescriptor(
+            DiagnosticId,
+            title: "Certificate callbacks should not always evaluate to true",
+            messageFormat: "Certificate callbacks should be overridden with specific validation criteria",
+            "Usage",
+            DiagnosticSeverity.Warning,
+            helpLinkUri: $"https://github.com/SkylineCommunications/Skyline.DataMiner.Utils.SecureCoding/blob/main/docs/Rules/{DiagnosticId}.md",
+            isEnabledByDefault: true);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.EnableConcurrentExecution();
+
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+
+            context.RegisterSyntaxNodeAction(AnalyzeUsages, SyntaxKind.SimpleAssignmentExpression);
+        }
+
+        public static void AnalyzeUsages(SyntaxNodeAnalysisContext context)
+        {
+            var assignmentExpression = (AssignmentExpressionSyntax)context.Node;
+
+            if (assignmentExpression.Left is MemberAccessExpressionSyntax memberAccess
+                && memberAccess.Name.Identifier.Text == "ServerCertificateCustomValidationCallback")
+            {
+                if (assignmentExpression.Right is ParenthesizedLambdaExpressionSyntax lambda)
+                {
+                    var alwaysReturnsTrue = lambda.Body is BlockSyntax block
+                        && block.Statements.OfType<ReturnStatementSyntax>()
+                        .All(r => r.Expression is LiteralExpressionSyntax literal && literal.IsKind(SyntaxKind.TrueLiteralExpression));
+
+                    if (alwaysReturnsTrue)
+                    {
+                        var diagnostic = Diagnostic.Create(Rule, memberAccess.Name.GetLocation());
+                        context.ReportDiagnostic(diagnostic);
+                    }
+                }
+
+                if (assignmentExpression.Right is IdentifierNameSyntax identifier)
+                {
+                    var methodSymbol = context.SemanticModel.GetSymbolInfo(identifier).Symbol as IMethodSymbol;
+                    if (methodSymbol != null)
+                    {
+                        var methodSyntax = methodSymbol.DeclaringSyntaxReferences[0].GetSyntax(context.CancellationToken) as MethodDeclarationSyntax;
+                        if (methodSyntax != null)
+                        {
+                            var alwaysReturnsTrue = methodSyntax.Body != null 
+                                && methodSyntax.Body.Statements.OfType<ReturnStatementSyntax>()
+                                .All(r => r.Expression is LiteralExpressionSyntax literal && literal.IsKind(SyntaxKind.TrueLiteralExpression));
+
+                            if (alwaysReturnsTrue)
+                            {
+                                var diagnostic = Diagnostic.Create(Rule, identifier.GetLocation(), methodSymbol.Name);
+                                context.ReportDiagnostic(diagnostic);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
