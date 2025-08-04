e19b8c83bd0980fcc95b2cc09f6859c959a8c0a1
Flávio Jacinto <flavio.jacinto@skyline.be>
2025-08-04 15:08:25 +0100

    Added BinaryFormatterAnalyzer

---
+++ Analyzers/Analyzers.Test/SecureSerialization/Binary/BinaryFormatterAnalyzerUnitTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.CodeAnalysis.Testing;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.CodeAnalysis;
+using Skyline.DataMiner.Utils.SecureCoding.Analyzers.SecureSerialization.Binary;
+
+namespace Skyline.DataMiner.Utils.SecureCoding.Analyzers.Tests.SecureSerialization.Binary
+{
+    [TestClass]
+    public class BinaryFormatterAnalyzerUnitTests
+    {
+        [TestMethod]
+        public async Task VerifyBinaryFormatterUsages()
+        {
+            var testCode = @"using System.Runtime.Serialization.Formatters.Binary;
+                            using System.IO;
+
+                            class TestClass
+                            {
+                                void TestMethod()
+                                {
+                                    var formatter = new BinaryFormatter();
+
+                                    formatter.Serialize(Stream.Null, new object());
+
+                                    formatter.Deserialize(Stream.Null);
+                                }
+                            }";
+
+            var expectedDiagnostics = new List<DiagnosticResult>()
+            {
+                AnalyzerVerifierHelper.BuildDiagnosticResult(BinaryFormatterAnalyzer.DiagnosticId, DiagnosticSeverity.Warning, 8, 53),
+                AnalyzerVerifierHelper.BuildDiagnosticResult(BinaryFormatterAnalyzer.DiagnosticId, DiagnosticSeverity.Warning, 10, 37),
+                AnalyzerVerifierHelper.BuildDiagnosticResult(BinaryFormatterAnalyzer.DiagnosticId, DiagnosticSeverity.Warning, 12, 37),
+            };
+
+            var analyzerVerifier = AnalyzerVerifierHelper.BuildAnalyzerVerifier<BinaryFormatterAnalyzer>(testCode);
+            analyzerVerifier.TestState.ExpectedDiagnostics.AddRange(expectedDiagnostics);
+
+            await analyzerVerifier.RunAsync();
+        }
+    }
+}
---
+++ Analyzers/Analyzers/SecureSerialization/Binary/BinaryFormatterAnalyzer.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Collections.Immutable;
+
+namespace Skyline.DataMiner.Utils.SecureCoding.Analyzers.SecureSerialization.Binary
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class BinaryFormatterAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "SLC_SC0008";
+
+        public static DiagnosticDescriptor Rule => new DiagnosticDescriptor(
+            DiagnosticId,
+            "Avoid deserializing data using BinaryFormatter.",
+            "BinaryFormatter serialization is obsolete and should not be used. See https://aka.ms/binaryformatter for more information.",
+            "Usage",
+            DiagnosticSeverity.Warning,
+            helpLinkUri: $"https://github.com/SkylineCommunications/Skyline.DataMiner.Utils.SecureCoding/blob/main/docs/Rules/{DiagnosticId}.md",
+            isEnabledByDefault: true
+         );
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.EnableConcurrentExecution();
+
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+
+            context.RegisterSyntaxNodeAction(AnalyzeBinaryFormatterObjectCreation, SyntaxKind.ObjectCreationExpression);
+            context.RegisterSyntaxNodeAction(AnalyzeBinaryFormatterInvocation, SyntaxKind.InvocationExpression);
+        }
+
+        private static void AnalyzeBinaryFormatterObjectCreation(SyntaxNodeAnalysisContext context)
+        {
+            var node = (ObjectCreationExpressionSyntax)context.Node;
+
+            var symbol = context.SemanticModel.GetSymbolInfo(node.Type).Symbol as INamedTypeSymbol;
+            if (symbol?.ToString() == "System.Runtime.Serialization.Formatters.Binary.BinaryFormatter")
+            {
+                context.ReportDiagnostic(Diagnostic.Create(Rule, node.GetLocation()));
+            }
+        }
+
+        private static void AnalyzeBinaryFormatterInvocation(SyntaxNodeAnalysisContext context)
+        {
+            var invocation = (InvocationExpressionSyntax)context.Node;
+
+            var symbol = context.SemanticModel.GetSymbolInfo(invocation).Symbol as IMethodSymbol;
+            if (symbol?.ContainingType.ToString() == "System.Runtime.Serialization.Formatters.Binary.BinaryFormatter")
+            {
+                context.ReportDiagnostic(Diagnostic.Create(Rule, invocation.GetLocation()));
+            }
+        }
+    }
+}
